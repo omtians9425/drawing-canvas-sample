d8b2a87f5a0ab4bd288d8f8b78cc92d5b52a5f28
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-04-14 19:20:04 +0900

    create custom view

--- app/src/main/java/com/example/minipaint/MainActivity.kt
+++ app/src/main/java/com/example/minipaint/MainActivity.kt
@@ -2,11 +2,16 @@ package com.example.minipaint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.SYSTEM_UI_FLAG_FULLSCREEN
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val myCanvasView = MyCanvasView(this).apply {
+            systemUiVisibility = SYSTEM_UI_FLAG_FULLSCREEN
+            contentDescription = getString(R.string.canvasContentDescription)
+        }
+        setContentView(myCanvasView)
     }
 }
